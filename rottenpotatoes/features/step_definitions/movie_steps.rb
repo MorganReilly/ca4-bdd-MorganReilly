110eb50c9132eb5a26f83939d1fca639fd5a9b71
Morgan Reilly <G00303598@gmit.ie>
2020-03-17 01:05:24 +0000

    Scenario: sort movies alphabetically passing

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -23,6 +23,9 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
+  if page.body.index(e2) < page.body.index(e1)
+    return fail "not sorted"
+  end
 end
 
 # Make it easier to express checking or unchecking several boxes at once
